54b34838c42a743b6d0a3609102789f8efe15f4c
Bishnu Charan Nayak <110585757+Bishnu-charan-nayak@users.noreply.github.com>
2026-03-20 20:46:31 +0530

    Add files via upload

---
+++ myMaltithread/Mytask.java
@@ -0,0 +1,35 @@
+package myMaltithread;
+
+public class Mytask {
+	public static void main(String[] args) {
+
+		System.out.println("Number.....");
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(500);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println(" ");
+		
+		for (int i = 65; i < 91; i++) {
+			System.out.print((char) i);
+		}
+
+		System.out.println("");
+		try {
+			Thread.sleep(500);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("2x" + i + "=" + (2 * 1));
+		}
+	}
+}

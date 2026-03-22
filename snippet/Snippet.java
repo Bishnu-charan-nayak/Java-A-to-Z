d9e4325c1cb51b3ad4aa5438c6725f2245cc886c
Bishnu Charan Nayak <110585757+Bishnu-charan-nayak@users.noreply.github.com>
2026-03-22 19:57:39 +0530

    Add files via upload

---
+++ snippet/Snippet.java
@@ -0,0 +1,11 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+				System.out.println(d);
+	}
+}
+

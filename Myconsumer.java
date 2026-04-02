2dcfaed45408eafeb343017a0ba2b0c4f578626b
Bishnu Charan Nayak <110585757+Bishnu-charan-nayak@users.noreply.github.com>
2026-04-02 16:24:40 +0530

    Add files via upload

---
+++ Myconsumer.java
@@ -0,0 +1,16 @@
+package myConsumer;
+
+public class Myconsumer {
+	public static void main(String[] args)  {
+		
+		for(i=1;i<=30;i++) {
+			if(i%3==0 && i%5==0) {
+				System.out.println("Fizz,Buzz");
+				
+			}
+		}
+			
+		}
+	}
+
+

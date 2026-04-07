0f0f49f253c4c1f756be12834a91303e8c5ae472
Bishnu Charan Nayak <110585757+Bishnu-charan-nayak@users.noreply.github.com>
2026-04-07 22:02:12 +0530

    Update MyMain7.java

--- myFuninterface/MyMain7.java
+++ myFuninterface/MyMain7.java
@@ -10,15 +10,31 @@ interface Opns{
 //	}
 //}
 
+// public class MyMain7 {
+//     public static void main(String[] args) {
+
+//         Opns obj = (int a, int b) -> {
+//             int c = a + b;
+//             System.out.println("Sum is " + c);
+//         };
+
+//         // calling method	
+//         obj.add(10, 20);
+//     }
+// }
+interface Opns {
+    int execute(int a, int b); // Changed to return int
+}
+
 public class MyMain7 {
     public static void main(String[] args) {
+        // Using lambda for addition
+        Opns add = (a, b) -> a + b;
+        
+        // Using the same interface for multiplication!
+        Opns multiply = (a, b) -> a * b;
 
-        Opns obj = (int a, int b) -> {
-            int c = a + b;
-            System.out.println("Sum is " + c);
-        };
-
-        // calling method	
-        obj.add(10, 20);
+        System.out.println("Addition: " + add.execute(10, 20));
+        System.out.println("Multiplication: " + multiply.execute(10, 20));
     }
 }

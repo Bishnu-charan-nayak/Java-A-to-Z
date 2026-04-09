e7c1c0d36fbb4e9a2bbc7edf66bf2bdfae2291c7
Bishnu Charan Nayak <110585757+Bishnu-charan-nayak@users.noreply.github.com>
2026-04-09 18:58:46 +0530

    Update MySupplier.java

--- myPredicate/MySupplier.java
+++ myPredicate/MySupplier.java
@@ -16,3 +16,24 @@ public class MySupplier {
         System.out.println("Generated OTP: " + s.get());
     }
 }
+
+
+
+/*More Example*/
+import java.util.function.Supplier;
+
+public class PasswordSupplier {
+    public static void main(String[] args) {
+        Supplier<String> passwordGen = () -> {
+            String symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+            StringBuilder pwd = new StringBuilder();
+            for (int i = 0; i < 8; i++) {
+                int index = (int) (Math.random() * symbols.length());
+                pwd.append(symbols.charAt(index));
+            }
+            return pwd.toString();
+        };
+
+        System.out.println("Suggested Password: " + passwordGen.get());
+    }
+}

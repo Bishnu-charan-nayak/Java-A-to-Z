f83a619e39c1bde683208922d5683774690a4cf0
Bishnu Charan Nayak <110585757+Bishnu-charan-nayak@users.noreply.github.com>
2026-04-10 22:34:16 +0530

    Update Mytask.java

--- Mytask.java
+++ Mytask.java
@@ -33,3 +33,59 @@ public class Mytask {
 		}
 	}
 }
+
+
+
+///// Other Example By using Thread
+
+// package myMaltithread;
+
+// // Task 1: Print Numbers
+// class NumberTask implements Runnable {
+//     @Override
+//     public void run() {
+//         for (int i = 1; i <= 10; i++) {
+//             System.out.println("Number: " + i);
+//             try {
+//                 Thread.sleep(500); // Pause for half a second
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
+
+// // Task 2: Print Alphabets
+// class AlphabetTask implements Runnable {
+//     @Override
+//     public void run() {
+//         for (int i = 65; i < 75; i++) { // Printing A to J to match the 10 numbers
+//             System.out.println("Alphabet: " + (char) i);
+//             try {
+//                 Thread.sleep(500);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
+
+// public class MultiThreadExample1 {
+//     public static void main(String[] args) {
+//         System.out.println("Starting both tasks simultaneously...\n");
+
+//         // Create the runnable tasks
+//         Runnable task1 = new NumberTask();
+//         Runnable task2 = new AlphabetTask();
+
+//         // Assign the tasks to actual Threads
+//         Thread thread1 = new Thread(task1);
+//         Thread thread2 = new Thread(task2);
+
+//         // Use .start() to run them at the same time
+//         // (Do NOT use .run(), as that will just run them sequentially!)
+//         thread1.start();
+//         thread2.start();
+//     }
+// }
+

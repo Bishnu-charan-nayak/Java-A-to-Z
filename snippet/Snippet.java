154ee52a19d0b947f2b242b0ae4fcd05e4238dae
Bishnu Charan Nayak <110585757+Bishnu-charan-nayak@users.noreply.github.com>
2026-04-10 22:31:20 +0530

    Update Snippet.java

--- snippet/Snippet.java
+++ snippet/Snippet.java
@@ -9,3 +9,23 @@ public class Snippet {
 	}
 }
 
+
+//Other example
+
+// package snippet;
+
+// public class Snippet {
+//     public static void main(String[] args) {
+//         // Declaring and initializing variables
+//         int a = 10;             // Integer
+//         double b = 20.5;        // Floating point
+//         char c = 'J';           // Character
+//         String d = "Java";      // String (text)
+
+//         // Printing the variables
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//         System.out.println(d);
+//     }
+// }
